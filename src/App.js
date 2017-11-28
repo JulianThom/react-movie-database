44e85ed443402eab83bb2e61e61bd184188b5b40
JulianThom <dieu.julian@gmail.com>
2017-11-28 09:21:37 +0100

    slideshow + navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/Navigation'
+import Slideshow from './components/Slideshow'
 
 class App extends Component {
-  render() {
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navigation />
+        <div className="wrapper">
+          <Slideshow />
+        </div>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { MenuItem,
+         NavDropdown,
+         NavItem,
+         Nav,
+         Navbar,
+         FormGroup,
+         FormControl,
+         Button }
+         from 'react-bootstrap';
+import './Navigation.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React MDB</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+            <Navbar.Form pullRight>
+              <FormGroup>
+                <FormControl type="text" placeholder="Search" />
+              </FormGroup>
+              <Button type="submit">Submit</Button>
+          </Navbar.Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Navigation;
