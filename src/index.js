3da9fc6466f63805f545c2bd78830bf9d1de59b0
JulianThom <dieu.julian@gmail.com>
2017-12-02 15:56:09 +0100

    install font-awesome

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './global.css';
 import App from './App';
+import 'font-awesome/css/font-awesome.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
