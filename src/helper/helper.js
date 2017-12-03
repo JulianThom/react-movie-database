3982c53786662b1d6ec2c1a4bbaa675906df1287
Julian <dieu.julian@gmail.com>
2017-12-03 16:55:13 +0100

    Feat.posters (#4)
    * display posters with requests
    
    * display row of content on home page

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,17 +1,21 @@
 const api_key = 'a8ccc2c614a1931b007aa9a92c8afb41';
 
+const base_url_api = 'https://api.themoviedb.org/3/';
+
 const base_url_backdrop_w780 = 'http://image.tmdb.org/t/p/w780/';
 const base_url_backdrop_w1280 = 'http://image.tmdb.org/t/p/w1280/';
 
 const base_url_poster_w342 = 'http://image.tmdb.org/t/p/w342/';
 const base_url_poster_w780 = 'http://image.tmdb.org/t/p/780/';
 
-
+const base_url_profile_sizes_w185 = 'http://image.tmdb.org/t/p/w185/'
 
 export {
   api_key,
+  base_url_api,
   base_url_backdrop_w780,
   base_url_backdrop_w1280,
   base_url_poster_w342,
-  base_url_poster_w780
+  base_url_poster_w780,
+  base_url_profile_sizes_w185
 };
