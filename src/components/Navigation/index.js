de0b1c4a9ddc4b7cb534dae6027f5da8a0841c66
JulianThom <dieu.julian@gmail.com>
2017-12-04 16:44:07 +0100

    set menu and icons

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -22,15 +22,9 @@ class Navigation extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
+            <NavItem eventKey={1} href="#"><i className="fa fa-ticket"></i> Movies</NavItem>
+            <NavItem eventKey={2} href="#"><i className="fa fa-television"></i> TV Shows</NavItem>
+            <NavItem eventKey={3} href="#"><i className="fa fa-user"></i> Actors</NavItem>
           </Nav>
             <Navbar.Form pullRight>
               <FormGroup>
