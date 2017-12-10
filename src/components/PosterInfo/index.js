286f82aa67b688053da84b65a63039e48725b0d1
Julian <dieu.julian@gmail.com>
2017-12-10 22:41:54 +0100

    add star rating component and display on posters (#6)

--- src/components/PosterInfo/index.js
+++ src/components/PosterInfo/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './index.css';
+var Rating = require('react-rating');
 
 class PosterInfo extends Component{
 
   render () {
     return (
       <div className="info">
-        {this.props.icon && <i className={`fa fa-${this.props.icon}`}></i>}
-        {this.props.value}
+        {
+          this.props.rating ?
+          <Rating
+            initialRate={this.props.value}
+            empty="fa fa-star-o"
+            full="fa fa-star"
+            readonly
+          />
+          : this.props.value
+        }
       </div>
     )
   }
