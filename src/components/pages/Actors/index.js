3f351d2c9b71664f75d55c7509e0fbd2045948f3
Julian <dieu.julian@gmail.com>
2017-12-13 12:28:56 +0100

    Feat.route navbar (#7)
    * navbar with link component and css reactor
    
    * add colors
    
    * add router and routes
    
    * create page components

---
+++ src/components/pages/Actors/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Actors extends Component{
+
+  render () {
+    return (
+      <div>
+        Actors Page!
+      </div>
+    )
+  }
+}
+
+export default Actors;
---
+++ src/components/pages/Movies/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Movies extends Component{
+
+  render () {
+    return (
+      <div>
+        Movies Page!
+      </div>
+    )
+  }
+}
+
+export default Movies;
