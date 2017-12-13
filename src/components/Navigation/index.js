1e3793a21b6c3ecce96583a1bd212a611712b646
JulianThom <dieu.julian@gmail.com>
2017-12-13 12:26:49 +0100

    navbar with link component and css reactor

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,37 +1,52 @@
 import React, { Component } from 'react';
-import { NavItem,
-         Nav,
-         Navbar,
+import { Navbar,
          FormGroup,
          FormControl,
          Button }
          from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 class Navigation extends Component {
   render() {
     return (
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">React MDB</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#"><i className="fa fa-ticket"></i> Movies</NavItem>
-            <NavItem eventKey={2} href="#"><i className="fa fa-television"></i> TV Shows</NavItem>
-            <NavItem eventKey={3} href="#"><i className="fa fa-user"></i> Actors</NavItem>
-          </Nav>
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">
+                RMDB
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <ul className="navList">
+              <li>
+                <Link to="/movies">
+                  <i className="fa fa-ticket"></i> Movies
+                </Link>
+              </li>
+              <li>
+                <Link to="/series">
+                    <i className="fa fa-television"></i> Series
+                </Link>
+              </li>
+              <li>
+                <Link to="/actors">
+                  <i className="fa fa-user"></i> Actors
+                </Link>
+              </li>
+            </ul>
             <Navbar.Form pullRight>
               <FormGroup>
                 <FormControl type="text" placeholder="Search" />
               </FormGroup>
               <Button type="submit">Submit</Button>
-          </Navbar.Form>
-        </Navbar.Collapse>
-      </Navbar>
+            </Navbar.Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
