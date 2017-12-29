375224104924d6ba983e15a0305ee36121efb71d
Julian <julian.dieu@ifeelsmart.com>
2017-12-29 11:57:29 +0100

    update config file

--- src/config.js
+++ src/config.js
@@ -3,5 +3,16 @@ export default {
     tv: 'tv',
     person: 'person',
     movie: 'movie'
+  },
+  tmdb: {
+    apiKey: 'a8ccc2c614a1931b007aa9a92c8afb41',
+    baseUrlApi: 'https://api.themoviedb.org/3/',
+    assets: {
+      baseUrlBackdropW780: 'http://image.tmdb.org/t/p/w780/',
+      baseUrlBackdropW1280: 'http://image.tmdb.org/t/p/w1280/',
+      baseUrlPosterW342: 'http://image.tmdb.org/t/p/w342/',
+      baseUrlPosterW780: 'http://image.tmdb.org/t/p/780/',
+      baseUrlProfileW185: 'http://image.tmdb.org/t/p/w185/'
+    }
   }
 }
