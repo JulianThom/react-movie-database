0ec773f74be03b96f7f48ce0c87315bb30f38ab4
Julian <julian.dieu@ifeelsmart.com>
2017-12-29 11:53:45 +0100

    rename pages components

---
+++ src/components/pages/Movie/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Movie extends Component{
+
+  render () {
+    return (
+      <div>
+        Movies Page!
+      </div>
+    )
+  }
+}
+
+export default Movie;
---
+++ src/components/pages/Tv/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Tv extends Component{
+
+  render () {
+    return (
+      <div>
+        Series Page!
+      </div>
+    )
+  }
+}
+
+export default Tv;
