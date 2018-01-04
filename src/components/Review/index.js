c96d2628e544097b787e7c4975e2bbee3c80cfd4
Julian <dieu.julian@gmail.com>
2018-01-04 22:47:01 +0100

    Fix.detail view (#26)
    * remove import config
    
    * rename css style
    
    * create a request function to display contents
    
    * install react-markdown to display correctly the reviews
    
    * remove previous css for ellipsis text
    
    * import markdown component
    
    * remove readMore props

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './index.css';
 
+const ReactMarkdown = require('react-markdown')
+
 class Review extends Component{
 
   render () {
     return (
       <div className="wrapperReview">
         <p className="author">{this.props.author}</p>
-        <p className="review">{this.props.review}</p>
-        <p className="readMore">
-          <a
-            href={this.props.readMore}
-            target="blank"
-          >
-             Read full review here
-          </a>.
-        </p>
+        <div className="review">
+          <ReactMarkdown
+            source={this.props.review}
+          />
+      </div>
       </div>
     )
   }
