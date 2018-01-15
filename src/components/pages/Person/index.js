c6ffebeca775d7f868ae3f5c76c75b133022bb21
Julian <julian.dieu@ifeelsmart.com>
2018-01-15 14:27:36 +0100

    add MainLayout on pages movies, series and actors

--- src/components/pages/Person/index.js
+++ src/components/pages/Person/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import MainLayout from '../../../components/layouts/MainLayout/'
+
 class Person extends Component{
 
   render () {
     return (
       <div>
-        Actors Page!
+        <MainLayout slideshow='false'>
+          <div>
+            Actors Page!
+          </div>
+        </MainLayout>
       </div>
     )
   }
