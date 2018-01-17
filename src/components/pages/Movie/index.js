b382e0ac78838f72fdf6664e12c03251982b45ce
Julian <dieu.julian@gmail.com>
2018-01-17 13:04:12 +0100

    Feat.movie page (#48)
    * install react-paginate
    
    * add layout and  pagination for movies
    
    * add select to filter contents
    
    * add margin on sort button
    
    * enable sort for movies
    
    * disable sort button when it's active and display text on sort buttons
    
    * change props of ReactPaginate
    
    * remove const desc and asc to pass those arguments directly to the function
    
    * add filter movie in config file
    
    * get filer movie to sort

--- src/components/pages/Movie/index.js
+++ src/components/pages/Movie/index.js
@@ -1,16 +1,171 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import RowPosters from '../../../components/RowPosters/'
+import Spinner from '../../../components/Spinner/'
 import MainLayout from '../../../components/layouts/MainLayout/'
+
+import axios from 'axios';
+import config from '../../../config'
+
+import ReactPaginate from 'react-paginate';
+
+const {
+  baseUrlApi,
+  apiKey
+} = config.tmdb;
 
 class Movie extends Component{
 
+  state = {
+    totalPages: '',
+    data: [],
+    offset: 1,
+    loading: true,
+    select: 'popularity',
+    sort: 'desc',
+    btnSortActive: 'active',
+  }
+
+  request = () => {
+    axios.get(`
+      ${baseUrlApi}discover/movie?api_key=${apiKey}&language=en-US
+      &sort_by=${this.state.select}.${this.state.sort}&include_adult=false&
+      include_video=false&page=${this.state.offset}
+      `)
+    .then(response => {
+      this.setState({
+        totalPages: response.data.total_pages,
+        data: response.data.results,
+        loading: false
+      })
+    })
+    .catch(error => {
+      console.log(error);
+      this.setState({
+        loading: false
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.request();
+  }
+
+  handlePageClick = (data) => {
+    let selected = data.selected;
+    let offset = Math.ceil(selected + 1);
+
+    this.setState({
+      offset: offset
+    }, () => {
+      this.request();
+    })
+  };
+
+  totalPages = () => {
+    if ( this.state.totalPages > 1000) {
+      return 1000
+    }else {
+      return this.state.totalPages
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      select: event.target.value,
+      offset: 1,
+    }, () => {
+      this.request();
+    })
+  }
+
+  handleSort = (arg) => {
+    this.setState({
+      sort: arg,
+      offset: 1,
+      btnSortActive: 'active'
+    }, () => {
+      this.request();
+    })
+  }
+
   render () {
+    const {
+      movie,
+    } = config.categories;
+
+    const movieSortBy = config.tmdb.sortMovie;
+
     return (
       <div>
+        {
+          this.state.loading && <Spinner />
+        }
         <MainLayout slideshow='false'>
-          <div>
-            Movies Page!
+            <form className="form-inline pull-right">
+              <div className="form-group">
+              <select
+                className="form-control"
+                style={{'width':'auto'}}
+                onChange={this.handleChange}
+              >
+                {
+                  Object.keys(movieSortBy).map(function(keyName, keyIndex) {
+                    return <option key={keyIndex} value={keyName}>{movieSortBy[keyName]}</option>
+                  })
+                }
+              </select>
+              <button
+                type="button"
+                className={
+                  this.state.btnSortActive && this.state.sort ==='desc'
+                  ? 'btn btn-primary disabled'
+                  : 'btn btn-primary'
+                }
+                onClick={() => this.handleSort('desc')}
+                >
+                  <i className="fa fa-sort-amount-desc">
+                  </i> Desc
+              </button>
+              <button
+                type="button"
+                className={
+                  this.state.btnSortActive && this.state.sort ==='asc'
+                  ? 'btn btn-primary disabled'
+                  : 'btn btn-primary'
+                }
+                onClick={() => this.handleSort('asc')}
+                >
+                  <i className="fa fa-sort-amount-asc">
+                  </i> Asc
+              </button>
+            </div>
+          </form>
+          <div className="wrapperRow">
+            <RowPosters
+              icon="ticket"
+              title="Movies"
+              contentToDisplay={18}
+              type={movie}
+              data={this.state.data}
+            />
+          <div className="wrapperPagination">
+              <ReactPaginate
+                previousLabel={"previous"}
+                nextLabel={"next"}
+                breakLabel={<a href="">...</a>}
+                breakClassName={"break-me"}
+                pageCount={this.totalPages()}
+                marginPagesDisplayed={1}
+                pageRangeDisplayed={9}
+                onPageChange={this.handlePageClick}
+                containerClassName={"pagination"}
+                subContainerClassName={"pages pagination"}
+                activeClassName={"active"}
+                forcePage={this.state.offset-1}
+              />
+            </div>
           </div>
         </MainLayout>
       </div>
