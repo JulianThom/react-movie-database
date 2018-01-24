42f5b5fc8ce096acaf89290b2a6afb050850126c
Julian <dieu.julian@gmail.com>
2018-01-24 17:13:06 +0100

    Refactor.row posters (#61)
    * refactor RowPosters component
    
    * refactor home component
    
    * remove useless var

--- src/components/RowPosters/index.js
+++ src/components/RowPosters/index.js
@@ -12,8 +12,6 @@ class RowPosters extends Component{
   render () {
 
     const {
-      movie,
-      tv,
       person
     } = config.categories;
 
@@ -33,66 +31,43 @@ class RowPosters extends Component{
     return (
       <div className="rows">
         {
-          icon && title ?
-          <RowTitle
-            icon={icon}
-            title={title}/>
-          : ''
+          icon && title
+          ?
+            <RowTitle
+              icon={icon}
+              title={title}
+            />
+          :
+            ''
         }
         <div className="rowPoster">
           <div className="containerPoster">
             {
-              cat === `${movie}` &&
               data.map((value, elem) => {
                 if ( elem < contentToDisplay ) {
                   return (
                     <Poster
                       key={elem}
                       icon="star"
-                      posterURL={`${baseUrlPosterW342}${value.poster_path}`}
+                      posterURL={
+                        cat === person
+                         ? `${baseUrlProfileW185}${value.profile_path}`
+                         : `${baseUrlPosterW342}${value.poster_path}`
+                       }
                       rate={value.vote_average}
-                      rating={true}
-                      value={rating(value.vote_average)}
+                      rating={cat === person ? false : true}
+                      value={
+                        cat === person
+                         ? value.name
+                         : rating(value.vote_average)
+                       }
                       id={value.id}
-                      cat={movie}
-                      imageStatus={value.poster_path}
-                    />
-                  )
-                }
-              })
-            }
-            {
-              cat === `${tv}` &&
-              data.slice(0, contentToDisplay).map((value, elem) => {
-                if ( elem < contentToDisplay ) {
-                  return (
-                    <Poster
-                      key={elem}
-                      icon="star"
-                      posterURL={`${baseUrlPosterW342}${value.poster_path}`}
-                      rating={true}
-                      value={rating(value.vote_average)}
-                      id={value.id}
-                      cat={tv}
-                      imageStatus={value.poster_path}
-                    />
-                  )
-                }
-              })
-            }
-            {
-              cat === `${person}` &&
-              data.slice(0, contentToDisplay).map((value, elem) => {
-                if ( elem < contentToDisplay ) {
-                  return (
-                    <Poster
-                      key={elem}
-                      posterURL={`${baseUrlProfileW185}${value.profile_path}`}
-                      value={value.name}
-                      rating={false}
-                      id={value.id}
-                      cat={person}
-                      imageStatus={value.profile_path}
+                      cat={this.props.cat}
+                      imageStatus={
+                        cat === person
+                         ? value.profile_path
+                         : value.poster_path
+                       }
                     />
                   )
                 }
